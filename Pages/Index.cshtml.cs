23cda6b8194f5efee7b13e0a1d5b9166974d0413
Thakkar Harshit (Consultant) <harshit.thakkar@consultant.volvo.com>
2022-08-05 16:12:54 +0530

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp1.Models;
+using WebApp1.Services;
+
+namespace WebApp1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public List<Product> Products;
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            ProductService sqlService = new ProductService();
+            Products = sqlService.GetProducts();
+        }
+    }
+}
---
+++ Services/ProductService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp1.Models;
+
+namespace WebApp1.Services
+{
+    public class ProductService
+    {
+        private string _dataSource = "dbserverharshit.database.windows.net";
+        private string _database = "DBHarshit";
+        private string _userId = "harshit";
+        private string _password = "India_2020";
+
+        private string GetSqlConnectionString()
+        {
+            var connectionStringBuilder = new SqlConnectionStringBuilder();
+            connectionStringBuilder.DataSource = _dataSource;
+            connectionStringBuilder.InitialCatalog = _database;
+            connectionStringBuilder.UserID = _userId;
+            connectionStringBuilder.Password = _password;
+            connectionStringBuilder.ConnectTimeout = 300;
+            return connectionStringBuilder.ConnectionString;
+        }
+
+        public List<Product> GetProducts()
+        {
+            var conn = new SqlConnection(GetSqlConnectionString());
+            conn.Open();
+
+            List<Product> products = new List<Product>();
+            string query = "select * from Products";
+            SqlCommand cmd = new SqlCommand(query, conn);
+
+            using (SqlDataReader dr = cmd.ExecuteReader())
+            {
+                while (dr.Read())
+                {
+                    var product = new Product()
+                    {
+                        ProductId = dr.GetInt32(0),
+                        ProductName = dr.GetString(1),
+                        Quantity = dr.GetInt32(2)
+                    };
+                    products.Add(product);
+                }
+            }
+
+            conn.Close();
+            return products;
+        }
+    }
+}
